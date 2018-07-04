3a7f5784fbde9d6a336cda17c3a9b8d6a1498b57
Johan Kok <johan.kok@ip-10-2-46-126.ap-southeast-1.compute.internal>
2018-07-04 19:32:31 +0800

    Implemented models class for Match table

--- ClientFrontEnd/MatchUpdater/models.py
+++ ClientFrontEnd/MatchUpdater/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class LatestMatchesData(models.Model):
+	league_id = models.IntegerField()
+	home_name = models.CharField(max_length=20, blank=False)
+	away_name = models.CharField(max_length=20, blank=False)
+	kick_off_time = models.DateTimeField(auto_now=False)
