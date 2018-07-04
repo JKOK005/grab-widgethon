f31a4b3a6d40f2a03002037915c949d7a7fad7b1
Johan Kok <johan.kok@ip-10-2-46-126.ap-southeast-1.compute.internal>
2018-07-04 19:19:03 +0800

    Added 2 apps to main settings.py file

--- ClientFrontEnd/ClientFrontEnd/settings.py
+++ ClientFrontEnd/ClientFrontEnd/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'MatchUpdater.apps.MatchupdaterConfig',
+    'TableUpdater.apps.TableupdaterConfig'
 ]
 
 MIDDLEWARE = [
