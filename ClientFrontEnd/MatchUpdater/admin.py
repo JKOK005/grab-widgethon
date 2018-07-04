778b10c7ef8cf1a0d9c419d66c93e5b85f07b85d
Johan Kok <johan.kok@ip-10-2-46-126.ap-southeast-1.compute.internal>
2018-07-04 19:45:45 +0800

    Enabled admin and description for match data

--- ClientFrontEnd/MatchUpdater/admin.py
+++ ClientFrontEnd/MatchUpdater/admin.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from MatchUpdater.models import *
 
 # Register your models here.
+class LatestMatchesDataAdmin(admin.ModelAdmin):
+	pass
+
+admin.site.register(LatestMatchesData, LatestMatchesDataAdmin)
--- ClientFrontEnd/MatchUpdater/models.py
+++ ClientFrontEnd/MatchUpdater/models.py
@@ -9,3 +9,6 @@ class LatestMatchesData(models.Model):
 	home_name = models.CharField(max_length=20, blank=False)
 	away_name = models.CharField(max_length=20, blank=False)
 	kick_off_time = models.DateTimeField(auto_now=False)
+
+	def __str__(self):
+		return self.home_name + "-vs-" + self.away_name
